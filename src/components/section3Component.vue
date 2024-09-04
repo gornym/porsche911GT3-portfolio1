73b32c2fa999479f29872ec3310da9050e9c6fd2
gornym <gornymaciek@gmail.com>
2024-09-04 23:13:41 +0200

    Poprawione kafelki na mobilki = proba druga

--- src/components/section3Component.vue
+++ src/components/section3Component.vue
@@ -431,6 +431,7 @@ export default {
       flex-direction: row;
       align-items: center;
       justify-content: center;
+      padding-bottom: 20px;
       .content-phone {
         height: 100%;
         width: 60%;
